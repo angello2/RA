daa953d14f7f9e4d564b60569c40c0987691a7df
Filip <angello2@github.com>
2022-12-07 21:52:22 +0100

    satisfied with smoke

--- RALab2/main.py
+++ RALab2/main.py
@@ -1,18 +1,29 @@
 from pyglet.gl import *
 from random import *
 import numpy as np
+import time
 
-window = pyglet.window.Window()
-tex = pyglet.image.load('smoke.bmp').get_texture()
+class SustavCesticaWindow(pyglet.window.Window):
+    def __init__(self, width=600, height=800):
+        super().__init__(width=width, height=height)
+        self.width = width
+        self.height = height
+        self.tex = pyglet.image.load('smoke.bmp').get_texture()
+        self.dim = sustav_cestica(np.array([width/2, 0.0, 0.0]), self.tex, self.width, self.height)
+
+    def on_draw(self):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClear(GL_COLOR_BUFFER_BIT)
+        self.dim.draw()
+
+    def get_dim(self):
+        return self.dim
 
 class cestica:
-
     def __init__(self, poz):
         self.poz = poz.copy()
-        self.poz[0] += gauss(0.0, 10.0)
-        self.poz[1] += gauss(-150.0, 50.0)
-        self.ds = np.array([uniform(-15.0, 15.0), uniform(50.0, 150.0), 0.0])
-        self.zivot = gauss(1500.0, 500.0)
+        self.ds = np.array([uniform(-15.0, 15.0), uniform(20.0, 100.0), 0.0])
+        self.zivot = gauss(3000.0, 1000.0)
         self.trenutni_zivot = self.zivot
         self.velicina = 25
 
@@ -23,10 +34,12 @@ class cestica:
 
 class sustav_cestica:
 
-    def __init__(self, broj_cestica, pocetni_polozaj):
+    def __init__(self, pocetni_polozaj, tex, width, height):
         self.cestice = []
         self.pocetni_polozaj = pocetni_polozaj
-        self.dodaj_cestice(broj_cestica)
+        self.tex = tex
+        self.width = width
+        self.height = height
 
     def dodaj_cestice(self, broj_cestica):
         for i in range(broj_cestica):
@@ -35,43 +48,43 @@ class sustav_cestica:
 
     def draw(self):
         glColor3f(1.0, 1.0, 1.0)
-        glEnable(tex.target)
-        glBindTexture(tex.target, tex.id)
+        glEnable(self.tex.target)
+        glBindTexture(self.tex.target, self.tex.id)
         glEnable(GL_BLEND)
         glBlendFunc(GL_ONE, GL_ONE)
         glBegin(GL_QUADS)
         for p in self.cestice:
-            velicina = p.velicina
             glTexCoord2f(0, 0)
-            glVertex3f(p.poz[0] - velicina, p.poz[1] - velicina, p.poz[2])
+            glVertex3f(p.poz[0] - p.velicina, p.poz[1] - p.velicina, p.poz[2])
             glTexCoord2f(1, 0)
-            glVertex3f(p.poz[0] + velicina, p.poz[1] - velicina, p.poz[2])
+            glVertex3f(p.poz[0] + p.velicina, p.poz[1] - p.velicina, p.poz[2])
             glTexCoord2f(1, 1)
-            glVertex3f(p.poz[0] + velicina, p.poz[1] + velicina, p.poz[2])
+            glVertex3f(p.poz[0] + p.velicina, p.poz[1] + p.velicina, p.poz[2])
             glTexCoord2f(0, 1)
-            glVertex3f(p.poz[0] - velicina, p.poz[1] + velicina, p.poz[2])
+            glVertex3f(p.poz[0] - p.velicina, p.poz[1] + p.velicina, p.poz[2])
         glEnd()
         glDisable(GL_BLEND)
-        glDisable(tex.target)
+        glDisable(self.tex.target)
 
     def update(self, dt):
         for i in range(len(self.cestice) - 1):
             c = self.cestice[i]
             c.update(dt)
-            if c.trenutni_zivot <= 0 or c.velicina <= 0.1 or c.poz[1] >= 500:
+            if c.trenutni_zivot <= 0 or c.velicina <= 0.1 or c.poz[1] >= self.height or c.poz[0] <= 0 or c.poz[0] >= self.width:
                 self.cestice[i] = cestica(self.pocetni_polozaj)
 
+    def get_num(self):
+        return len(self.cestice)
 
-dim = sustav_cestica(500, np.array([300.0, 0.0, 0.0]))
+if __name__ == '__main__':
+    window = SustavCesticaWindow()
 
-@window.event
-def on_draw():
-    glClearColor(0, 0, 0, 0)
-    glClear(GL_COLOR_BUFFER_BIT)
-    dim.draw()
+    def update(dt):
+        dim = window.get_dim()
+        if(dim.get_num() < 1000):
+            dim.dodaj_cestice(1)
 
-def update(dt):
-    dim.update(dt)
+        dim.update(dt)
 
-pyglet.clock.schedule_interval(update, 1.0/1000.0)
-pyglet.app.run()
+    pyglet.clock.schedule_interval(update, 1.0/120.0)
+    pyglet.app.run()
